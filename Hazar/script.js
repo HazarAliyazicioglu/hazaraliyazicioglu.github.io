2d8e855e6138a9ff202bb16b753c0c77aa2d5d6c
Hazar Aliyazıcıoğlu <106108977+HazarAliyazicioglu@users.noreply.github.com>
2024-08-14 22:53:22 +0300

    Delete Hazar directory

--- Hazar/script.js
+++
@@ -1,9 +0,0 @@
-
-
-document.addEventListener('visibilitychange', function() {
-    if (document.visibilityState === 'hidden') {
-        document.title = 'Aradığın portföy burada :)';
-    } else {
-        document.title = "Hazar's Portfolio";
-    }
-});
